5ac790e73b65dddd699628a15ccd05bb7e0ae965
栄佑 <hero.naga0706@gmail.com>
2020-05-05 22:11:54 +0900

    titlise

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     this.state=DEFAULT_STATE;
 
     this.handleChange=this.handleChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
   }
 
   handleChange(event){
@@ -26,12 +27,16 @@ class App extends Component {
       query:event.target.value
     })
   }
+
+  handleSubmit(event){
+    event.preventDefault();
+  }
   render(){
     const {query,first,last,before,after}=this.state;
     console.log({query})
     return (
       <ApolloProvider client={client}>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input　value={query} onChange={this.handleChange}　/>
         </form>
         <Query
@@ -42,8 +47,13 @@ class App extends Component {
             ({loading,error,data})=>{
               if(loading) return 'Loading...'
               if(error) return `Error! ${error.message}`
-              console.log(data);
-              return <div>{}</div>
+
+              console.log(data.search);
+              const search=data.search;
+              const repositoryCount=search.repositoryCount;
+              const repositoryUnit=repositoryCount === 1 ? 'Repository' : 'Repositories';
+              const title=`Github Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
+            return <h2>{title}</h2>
             }
           }
         </Query>
