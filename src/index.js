dbac854d59e19258906f3a78ee7153eba2fefee6
栄佑 <hero.naga0706@gmail.com>
2020-05-05 13:03:35 +0900

    setup dotenv

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const GITHUB_TOKEN=process.env.REACT_APP_GITHUB_TOKEN;
+
+console.log({GITHUB_TOKEN})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
